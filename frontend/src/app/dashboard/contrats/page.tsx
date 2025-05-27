8483d9353a355271ed217d9de78a18459745b2f4
easyentrepreneur <bonaldithomaspro@gmail.com>
2025-05-27 19:29:22 +0200

    new changes

---
+++ frontend/src/app/dashboard/contrats/page.tsx
@@ -0,0 +1,4 @@
+// Exemple : src/app/dashboard/contrats/page.tsx
+export default function Contrats() {
+  return <div>Page en construction</div>;
+}
---
+++ frontend/src/app/dashboard/support/page.tsx
@@ -0,0 +1,4 @@
+// Exemple : src/app/dashboard/contrats/page.tsx
+export default function Support() {
+  return <div>Page en construction</div>;
+}
