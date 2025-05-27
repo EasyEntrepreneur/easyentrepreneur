090e60d830b04b90ed1779ad1a45666c6f4af117
easyentrepreneur <bonaldithomaspro@gmail.com>
2025-05-27 18:33:56 +0200

    Update prisma.ts

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -5,9 +5,12 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: { db: { url: process.env.DATABASE_URL } },
-  });
+  (() => {
+    console.log('✨ Nouvelle instance PrismaClient créée');
+    return new PrismaClient({
+      datasources: { db: { url: process.env.DATABASE_URL } },
+    });
+  })();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
