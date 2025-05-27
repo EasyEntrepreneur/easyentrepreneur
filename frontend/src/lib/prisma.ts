0f2dd8d8f243502e4269e553df049176802f5ed2
easyentrepreneur <bonaldithomaspro@gmail.com>
2025-05-27 18:33:07 +0200

    Update prisma.ts

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -1,5 +1,14 @@
-// backend/src/lib/prisma.ts
+// frontend/src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL } },
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
 export default prisma;
